d427c7d6dada1fd42f3f84847da996566063f8c2
Rabbit <Corbineau@users.noreply.github.com>
2019-05-15 23:55:42 -0400

    WIP: adding prompt

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
+
+inquirer.prompt({
+    type: "list",
+    message: "what would you like to buy?",
+    choices: [],
+    name: "options"
+})
