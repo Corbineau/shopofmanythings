74a86098bdcd4d537076df724a182ab6ed97d877
Rabbit <Corbineau@users.noreply.github.com>
2019-05-21 00:51:54 -0400

    getting fancy

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
     host: config.dbHost,
     port: config.port,
     user: config.dbUser,
-    password: config.dbUser,
+    password: config.pass,
     database: config.db
   });
   
@@ -17,12 +17,54 @@ var connection = mysql.createConnection({
     start();
   });
 
+  //shopping cart? I don't know if I have the wherewithal, but perhaps something like...
+
+  var cart = {
+    itemCount: 0,
+    totalPrice: 0,
+    items: [],
+
+    addItem: function(item) {
+      totalPrice += (item.price * item.quantity);
+      this.items.push(item);
+    },
+
+    removeItem: function(item) {
+      for(let i = 0; i < this.items.length; i++) {
+        if(this.items[i] === item){
+          this.items.splice(i, 1);
+        }
+      }
+
+    }
+
+  }
+
+  //validation to make sure that the user selects a valid quantity
+  const inStock = (num, ans) => {
+    if(typeof num != Number) {
+      console.log("Please enter a valid quantity.");
+    } else {
+      connection.query("SELECT quantity FROM products WHERE ?", {product_id: ans.item}, function(err, res) {
+        if(res < num) {
+          console.log("I'm sorry, we don't have that many! Please enter a lower quantity.");
+          //it'd be cool to also have this alert the manager to order more. 
+        } else {
+          return true
+        }
+      })
+    }
+
+    }
+
+
   var start = function() {
     connection.query("SELECT * FROM products", function(err, results) {
         if (err) throw err;
+     var cart;   
     inquirer.prompt({
         type: "rawlist",
-        message: "what would you like to buy?",
+        message: "Select the ID of the item you'd like to purchase",
         choices: function() {
             var choiceArray = [];
             for (var i = 0; i < results.length; i++) {
@@ -30,7 +72,39 @@ var connection = mysql.createConnection({
             }
             return choiceArray;
           },
-        name: "options"
-    })
+        name: "item"
+    },
+    {
+      type: "input",
+      message: "how many do you want?",
+      name: "quantity",
+      validate: inStock
+    }).then(function(ans){
+      //find the chosen item and get its relevant features
+      let query = "SELECT product_name, price, quantity FROM products WHERE ?";
+      connection.query(query, {product_id : ans.item}, function(err, res) {
+        let newItem = {
+          name: res.product_name,
+          price: res.price,
+          quantity: ans.quantity
+        }
+      })
+    }).catch(function(err){
+      throw err;
+    }).then(function(res) {
+      //confirm the purchase
+      let total = (res.price * quant);
 
-  }
+      inquirer.prompt([
+          {
+            type: "confirm",
+            message: `you have selected ${item} X ${quant}, for a total price of ${total}. Is this correct?`,
+            name: "finalSale"
+          }
+        ]) 
+
+    }
+      
+    )
+
+    })}
