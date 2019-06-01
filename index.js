23c9e38dde6b39cf9e119d61e319f05641033d1f
Rabbit <Corbineau@users.noreply.github.com>
2019-06-01 12:26:09 -0400

    let's get everything

--- index.js
+++ index.js
@@ -21,11 +21,12 @@ const table = new Table({
   , colWidths: [10, 25, 25, 25, 10, 15]
 });
 
+//array so that I'm not having to constantly query unless something changes in the database.
 var products = [];
 
 
 
-//shopping cart? I don't know if I have the wherewithal, but perhaps something like...
+//shopping cart. Went this way because it's easier to refer back to the items in the object.
 
 var cart = {
   totalPrice: 0,
@@ -53,7 +54,7 @@ var cart = {
   },
 
   tallyItem: function (item) {
-   return (item.quantity * item.price)
+    return (item.quantity * item.price)
   },
 
   tallyCart: function () {
@@ -72,13 +73,13 @@ var cart = {
 
   showCart: new Table({
     head: ['item', 'price', 'quantity', 'total']
-    , colWidths: [100, 100, 100, 100]
+    , colWidths: [25, 25, 25, 25]
   }),
 
   fillCart: function () {
     for (var i = 0; i < this.items.length; i++) {
       this.showCart.push(
-        [this.items[i].name, this.items[i].price, this.items[i].quantity, tallyItem(items[i]) ]
+        [this.items[i].name, this.items[i].price, this.items[i].quantity, this.tallyItem(items[i])]
       );
     };
     console.log(showCart.toString());
@@ -88,12 +89,13 @@ var cart = {
 
 //validation to make sure that the user selects a valid quantity
 const inStock = (num, ans) => {
-  if (typeof num != Number) {
-    console.log("Please enter a valid quantity.");
+  if (isNaN(num)) {
+    console.log("\n Please enter a valid quantity.");
   } else {
-    connection.query("SELECT quantity FROM products WHERE ?", { product_id: ans.item }, function (err, res) {
+    connection.query("SELECT stock_quantity FROM products WHERE (?)", { product_id: ans.item }, function (err, res) {
+      if (err) throw err;
       if (res < num) {
-        console.log("I'm sorry, we don't have that many! Please enter a lower quantity.");
+        console.log("\n I'm sorry, we don't have that many! Please enter a lower quantity.");
         //it'd be cool to also have this alert the manager to order more. 
       } else {
         return true
@@ -106,7 +108,7 @@ const inStock = (num, ans) => {
 const isItem = (input) => {
   num = parseInt(input);
   let check = products.find(product => product.item_id === num);
-  if(check) {
+  if (check) {
     return true
   } else {
     console.log(`\n please enter a valid item id.`)
@@ -115,129 +117,99 @@ const isItem = (input) => {
 }
 
 
-
-  // if(!valid) {
-  //   console.log("please enter a valid item id.")
-  // }
-  
-
-
-
-
 // connect to the mysql server and sql database
 connection.connect(function (err) {
   if (err) throw err;
   // run the start function after the connection is made to initialize everything.
   console.log("connected!");
   start();
-  // connection.end();
+
 });
 
 const start = function () {
   cart.empty();
-  connection.query("SELECT * FROM products", function (err, res) {
-    if (err) throw err;
-    products = res;
-    for (let i = 0; i < res.length; i++) {
+    showProd();
+    store();
+  }
+
+
+  const showProd = () => {
+    connection.query("SELECT * FROM products", function (err, res) {
+      if (err) throw err;
+      products = res;
+      for (let i = 0; i < res.length; i++) {
         table.push(
           [res[i].item_id, res[i].product_name, res[i].product_desc, res[i].department_name, res[i].price, res[i].stock_quantity]
         );
-    };
+      };
       console.log(table.toString());
-    // connection.end();
-    console.log(products);
-    store();
-  })
+    })
+  }
 
 
-// const viewAll = () => {
-//   connection.query("SELECT * FROM products", function (err, res) {
-//     if (err) throw err;
-//     // build a pretty table
-//     for (var i = 0; i < res.length; i++) {
-//       table.push(
-//         [res[i].item_id, res[i].product_name, res[i].product_desc, res[i].department_name, res[i].price, res[i].stock_quantity]
-//       );
-//     };
-//     console.log(table.toString());
-//   })
-// }
-
-const showTable = (p) => {
-  
-}
-
-var search = () => {
-  };
- 
-    
-
-
-
-const store = () => {
-  inquirer.prompt({
-    type: "input",
-    message: `Select the ID of the item you'd like to purchase:`,
-    // choices: function () {
-    //   var choiceArray = [];
-    //   for (var i = 0; i < products.length; i++) {
-    //     // console.log(products);
-    //     choiceArray.push(products[i].item_id);
-    //   }
-    //   return choiceArray;
-    // },
-    name: "item",
-    validate: isItem
-  },
-    {
-      type: "input",
-      message: "how many do you want?",
-      name: "quantity",
-      validate: inStock
-    }).then(function (ans) {
-      //find the chosen item and get its relevant features
-      let query = "SELECT product_name, price, stock_quantity FROM products WHERE ?";
-      connection.query(query, { product_id: ans.item }, function (res) {
-        let newItem = {
-          name: res.product_name,
-          price: res.price,
-          quantity: ans.quantity
-          //figure out where to add the functionality to change the quantity of an item in the cart and make the price changes
-        }
-        cart.addItem(newItem);
-        return newItem
-      })
-    }).catch(function (err) {
-      throw err;
-    }).then(function (res) {
-      //ask if they want more stuff
-      inquirer.prompt([
-        {
-          type: "confirm",
-          message: "would you like to add anything else?",
-          name: "check"
-        }
-      ]).then(function (ans) {
-        if (ans.check) {
-          store();
-        } else {
-          inquirer.prompt([
-            {
-              type: "confirm",
-              message: `you have ${cart.itemCount} items in your cart: \n
+  const store = () => {
+    inquirer.prompt([
+      {
+        type: "input",
+        message: `Select the ID of the item you'd like to purchase:`,
+        name: "item",
+        validate: isItem
+      },
+      {
+        type: "input",
+        message: "how many do you want?",
+        name: "quantity",
+        validate: inStock
+      }]).catch(function (err) {
+        throw err;
+      }).then(function (ans) {
+        //find the chosen item and get its relevant features
+        let query = "SELECT product_name, price, stock_quantity FROM products WHERE item_id = ?";
+        connection.query(query, { item_id: ans.item }, function (res) {
+          let newItem = {
+            name: res.product_name,
+            price: res.price,
+            quantity: ans.quantity
+            //figure out where to add the functionality to change the quantity of an item in the cart and make the price changes
+          }
+          cart.addItem(newItem);
+          console.log(cart.items);
+          return newItem
+        })
+      }).catch(function (err) {
+        throw err;
+      }).then(function (res) {
+        //ask if they want more stuff
+        inquirer.prompt([
+          {
+            type: "confirm",
+            message: "would you like to add anything else?",
+            name: "check"
+          }
+        ]).then(function (ans) {
+          if (ans.check) {
+            store();
+          } else {
+            inquirer.prompt([
+              {
+                type: "confirm",
+                message: `you have ${cart.itemCount} items in your cart: \n
               ${cart.fillCart()}/n
               For a total of ${cart.tallyCart}. \n  
               Would you like to make changes?`,
-              name: "total"
-            }
-          ]).then(function(ans){
+                name: "total"
+              }
+            ]).then(function (ans) {
+              if(total){
+                //update DB to remove thingies.
+              }
 
-          })
+            })
 
-        }
+          }
+
+        })
+
 
       })
-    
-
-})
-  }}
+  }
